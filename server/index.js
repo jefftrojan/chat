36024cf4eae5c0592d992c946fccb7d9a70a7e55
Jeff Trojan <j.dauda@alustudent.com>
2023-02-06 02:32:29 +0200

    Added connection to socket.io

--- server/index.js
+++ server/index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
+const socket = require('socket.io');
 const cors = require('cors');
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
+
+
 
 const port = 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Application server is running on port ${port}`);
 });
+
+// create connection to socket
+io = socket(server)
+
+io.on('connection', (socket)=>{
+    console.log(socket.id);
+
+    socket.on('join_room', (data)=>{
+        socket.join(data)
+        console.log('User Joined Room: ' + data)
+    })
+
+    socket.on('disconnect', ()=>{
+        console.log('User disconnected');
+    })
+})
